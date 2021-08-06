d0e59b29d1637004c5c016e9a03eb134a38151da
Muhammed <shafafmuhammad6@gmail.com>
2021-08-06 14:45:57 +0530

    MVT config completed

---
+++ todo/tasks/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from .models import *
+
+# Create your views here.
+
+def index(request):
+    tasks = Task.objects.all()
+
+    context = {'tasks':tasks}
+    return render(request, 'tasks/list.html',context)
