099af68c7f2448d3f880fe112fa2ae96365f6a58
Muhammed <shafafmuhammad6@gmail.com>
2021-08-07 14:06:20 +0530

    Final

--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from .models import *
 from .forms import *
+
+
 # Create your views here.
 
 def index(request):
@@ -14,14 +16,33 @@ def index(request):
             form.save()
         return redirect('/')
 
-    context = {'tasks':tasks, 'form':form}
-    return render(request, 'tasks/list.html',context)
+    context = {'tasks': tasks, 'form': form}
+    return render(request, 'tasks/list.html', context)
 
 
 def updateTask(request, pk):
     task = Task.objects.get(id=pk)
 
     form = TaskForm(instance=task)
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
     context = {'form': form}
 
     return render(request, 'tasks/update_task.html', context)
+
+
+def deleteTask(request, pk):
+    item = Task.objects.get(id=pk)
+
+    if request.method == 'POST':
+        item.delete()
+        return redirect('/')
+
+    context = {'item': item}
+
+    return render(request, 'tasks/delete.html', context)
